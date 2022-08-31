4ea4e5217cb9a00228402578a66061607747bff9
Antonio Ruiz Benito <109352678+SpeeDemon3@users.noreply.github.com>
2022-08-31 19:14:29 +0200

    Add files via upload

---
+++ bucle-texto/src/ruiz/antonio/BucleTexto.java
@@ -0,0 +1,15 @@
+package ruiz.antonio;
+
+public class BucleTexto {
+
+    public static void main(String[] args) {
+        String[] animales = new String[]{"Perro", "Gato", "Leon", "Aguila", "Tiburon"};
+
+        for(int i = 0; i < animales.length; i++) {
+            System.out.print(animales[i] + " ");
+        }
+
+    }
+}
+
+
